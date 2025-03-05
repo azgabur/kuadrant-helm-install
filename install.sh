c27176aeb15d813bb81121269d9c1b918e789125
Tomas Repel <trepel@redhat.com>
2025-03-05 09:06:16 +0100

    Simplify RHCL GA install

--- install.sh
+++ install.sh
@@ -5,16 +5,20 @@ cd "$(dirname "$0")" || exit 1
 
 additional_flags=''
 
-if [[ "$1" = "-t" ]]; then
-    additional_flags+="--values additionalManifests.yaml --set tools.enabled=true"
+if [[ "$1" == "-t" ]]; then
+    additional_flags+=" --values additionalManifests.yaml --set tools.enabled=true"
+fi
+
+if [[ "$INSTALL_RHCL_GA" == "true" ]]; then
+    additional_flags+=" --set kuadrant.indexImage='' --set kuadrant.operatorName=rhcl-operator --set kuadrant.channel=stable"
 fi
 
 echo "---Installing operators---"
-helm install --values values.yaml $additional_flags \
-  --wait kuadrant-operators operators/
+helm_cmd="helm install --values values.yaml $additional_flags --wait kuadrant-operators operators/"
+eval "$helm_cmd"
 
 echo "--Installing instances---"
-helm install --values values.yaml $additional_flags \
-  --wait kuadrant-instances instances/
+helm_cmd="helm install --values values.yaml $additional_flags --wait kuadrant-instances instances/"
+eval "$helm_cmd"
 
 echo "Success!"
